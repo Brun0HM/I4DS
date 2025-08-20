811d32dcfb5ac0f259257d5e308a364a5a43d162
Brun0 <brunodev41@gmail.com>
2025-08-20 09:30:39 -0300

    Inciando o header

--- IOT/DashBoard-02/src/App.jsx
+++ IOT/DashBoard-02/src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="container-fluid py-4">
+      <div className="row justify-content-center">
+        <div className="col-12 col-xl-10">
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

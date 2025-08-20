c8ce0a7d0b784ce21571c8d873681f2f3367dffd
Brun0 <brunodev41@gmail.com>
2025-08-20 11:53:09 -0300

    retirando a extensao

--- IOT/DashBoard-02/src/main.jsx
+++ IOT/DashBoard-02/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
 import App from "./App.jsx";
-import { ThemeProvider } from "./contexts/themeContext.jsx";
+import { ThemeProvider } from "./contexts/themeContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
